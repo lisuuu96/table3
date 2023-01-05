82374c20358a84cd85ab1b969aebb60758d5a830
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-05 09:20:08 +0100

    co

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let table = [1,2,3,4,5]
-function array (table){return table[Element(1)]}
-function array(table)
-console.log(array)(table)
+function array (table){
+    return table[1]
+}
+
+console.log(array(table));
 
 
