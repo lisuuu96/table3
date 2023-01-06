684c4610263fa6d746a24ebace2198b8edb6f2bb
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-06 16:51:25 +0100

    überprüfung

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let table = [1,2,3,4,5]
-function array (table){
-    return table[1]
+let tab = [1,2,3,4,5]
+function array (tab){
+    return tab[0]
 }
 
-console.log(array(table));
+console.log(array(tab));
 
 
